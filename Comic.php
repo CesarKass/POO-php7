73534df2690709702f132cb2a613d66a0efc4b50
Cesar Acuña <cesar.acu@hotmail.com>
2022-08-11 14:12:28 -0500

    Polimorfismo a comic

--- Comic.php
+++ Comic.php
@@ -1,10 +1,27 @@
 <?php
 require 'Book.php';
 class Comic extends Book { 
+    private array $imagen;
+    private int $volumen; 
     public function __construct($author, $title, $price, $stock, $id, $imagen, $volumen)
     { 
         parent::__construct($author, $title, $price, $stock, $id ); 
+        $this->imagen = $imagen;
+        $this->volumen = $volumen;
     } 
+    
+    public function getInfo()
+    {
+        $template = parent::getInfo();
+        $template .= "<br>";
+        $template .= "<b> Volumen: </b> {$this->volumen} <br>";
+        $template .= "<ul> <b> - Imagenes - </b>";
+        foreach ($this->imagen as $value) {
+            $template .= "<li> $value </li>";
+        }
+        $template .= "</ul>";
+        return $template;
+    }
 }
 
 $comic1= new Comic(
